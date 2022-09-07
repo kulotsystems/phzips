e0d07a02d0ede58331780dd677dbea9472174584
Arvic S. Babol <kulotsystems@gmail.com>
2022-09-07 12:59:21 +0800

    Include detailed zip codes for Metro Manila.

--- src/zip/index.js
+++ src/zip/index.js
@@ -1,23 +1,51 @@
-import provinces from './data/provinces.json';
-import cities    from './data/cities.json';
-import zipcodes  from './data/zipcodes.json';
+import metroManila           from './data/metro-manila.json';
+import metroManila_locations from './data/metro-manila_locations.json';
+import metroManila_zipcodes  from './data/metro-manila_zipcodes.json'
+import provinces             from './data/provinces.json';
+import province_locations    from './data/province_locations.json';
+import province_zipcodes     from './data/province_zipcodes.json'
+
+const find = (code, inProvince) => {
+    let areas     = metroManila;
+    let locations = metroManila_locations;
+    let zipcodes  = metroManila_zipcodes;
+    if(inProvince) {
+        areas = provinces;
+        locations = province_locations;
+        zipcodes  = province_zipcodes;
+    }
+
+    if(zipcodes[code] && code !== '') {
+        const zip        = zipcodes[code];
+        const areaID     = zip[0];
+        const category   = zip[1];
+        const locationID = zip[2];
+
+        const area     = areas[areaID];
+        const location = locations[areaID][category][locationID];
+
+        const data = {
+            type: inProvince ? 'Province' : 'Metro Manila',
+            area,
+            location: location[0],
+            zipCode : location[1],
+            tag     : location[2] ? location[2] : null
+        };
+
+        return data;
+    }
+    return false;
+};
 
 export default {
+
     search(code) {
         code = code.trim();
-        if(zipcodes[code] && code !== '') {
-            const zip = zipcodes[code];
-            const provinceID = zip[0];
-            const cityID     = zip[1];
-
-            const province = provinces[provinceID];
-            const city     = cities[provinceID][cityID];
-            return {
-                province,
-                city   : city[0],
-                zipCode: city[1]
-            }
-        }
-        return false;
+        const foundInMetroManila = find(code, false);
+        if(foundInMetroManila)
+            return foundInMetroManila;
+        else
+            return find(code, true);
     }
+
 }
